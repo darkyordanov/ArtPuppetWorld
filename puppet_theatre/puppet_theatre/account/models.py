50377edfbc5b2db4dad4fda6d0f55eaa8ed1ad15
Yordan Yordanov (S. T. G.) <y.yordanov.stg@gmail.com>
2024-05-19 00:28:27 +0300

    Updated project name to 'Art Puppet World'

--- puppet_theatre/puppet_theatre/account/models.py
+++ puppet_theatre/puppet_theatre/account/models.py
@@ -1,3 +1,73 @@
 from django.db import models
+from django.contrib.auth import models as auth_models
 
-# Create your models here.
+from django.core.mail import send_mail
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from puppet_theatre.common.model_mixins import TimeStampedModel
+from puppet_theatre.account.managers import AccountUserManager
+
+
+class AccountUser(auth_models.AbstractBaseUser, auth_models.PermissionsMixin, TimeStampedModel):
+    email = models.EmailField(
+        unique=True,
+        error_messages={
+            "unique": _("A user with that email already exists."),
+        },
+    )
+        
+    first_name = models.CharField(
+        _("first name"),
+        max_length=150,
+        blank=True
+    )
+    
+    last_name = models.CharField(
+        _("last name"),
+        max_length=150,
+        blank=True
+    )
+              
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+    
+    USERNAME_FIELD = "email"
+    
+    objects = AccountUserManager()
+    
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = "%s %s" % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """Return the short name for the user."""
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+        
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
